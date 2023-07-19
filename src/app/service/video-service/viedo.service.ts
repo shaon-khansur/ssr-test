7a5d8823d10578d430bf387751bc4178472c7608
shaonkhansur <shaon@npcoding.com>
2023-07-20 04:14:22 +0600

    environment added

--- src/app/service/video-service/viedo.service.ts
+++ src/app/service/video-service/viedo.service.ts
@@ -11,21 +11,19 @@ import {
   toArray,
 } from 'rxjs';
 import { Video } from 'src/app/models/video';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ViedoService {
-  private baseURL: string =
-    'https://gist.githubusercontent.com/poudyalanil/ca84582cbeb4fc123a13290a586da925/raw/14a27bd0bcd0cd323b35ad79cf3b493dddf6216b/videos.json';
-
   subject = new BehaviorSubject<Video[]>([]);
   videos$ = this.subject.asObservable();
 
   constructor(private http: HttpClient) {}
 
   getVideos(): Observable<Video[]> {
-    return this.http.get<Video[]>(`${this.baseURL}`);
+    return this.http.get<Video[]>(`${environment.baseURL}`);
   }
 
   getVideoById(id: string): Observable<Video> {
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  baseURL: "https://gist.githubusercontent.com/poudyalanil/ca84582cbeb4fc123a13290a586da925/raw/14a27bd0bcd0cd323b35ad79cf3b493dddf6216b/videos.json"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
